c16a03c8433c7ba0d088bd247550545845063f56
josh <jbothun@concentricsky.com>
2013-01-08 15:00:45 -0800

    add expected fields tests

--- basic_models/tests.py
+++ basic_models/tests.py
@@ -1,2 +1,13 @@
 from django.test import TestCase
 
+from test_project.models import Category
+
+
+class ActiveModelTestCase(TestCase):
+    def test_expected_fields_exist(self):
+        category = Category(name='foo')
+        self.assertTrue(hasattr(category, 'is_active'))
+
+    def test_expected_defaults(self):
+        category = Category(name='bar')
+        self.assertTrue(category.is_active)
--- tests/test_project/models.py
+++ tests/test_project/models.py
@@ -9,8 +9,11 @@ class Category(basic_models.SlugModel):
     pass
 
 class Post(basic_models.SlugModel):
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, null=True, blank=True)
     body     = models.TextField()
+
+    def create_comment(self, **kwargs):
+        return Comment.objects.create(post=self, **kwargs)
 
 class Comment(basic_models.DefaultModel):
     post = models.ForeignKey(Post)
